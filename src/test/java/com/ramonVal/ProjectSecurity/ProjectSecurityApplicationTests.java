729a9c8e3753efe34bacfff44b69388e1af5b321
Ramon Valenzuela <rvalenzuela@frba.utn.edu.ar>
2026-03-11 09:16:14 -0300

    base de proyecto con sus dependencias en el pom

---
+++ src/test/java/com/ramonVal/ProjectSecurity/ProjectSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ramonVal.ProjectSecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
